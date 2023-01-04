548a801535892fd088c774a1db493fdf08eefaa0
DanielaVoiculescu <daniela.voiculescu@student.upt.ro>
2023-01-04 19:01:06 +0200

    Detailed announcements and requests

--- app/src/main/java/com/example/boltdogapp/ViewRequests.java
+++ app/src/main/java/com/example/boltdogapp/ViewRequests.java
@@ -1,18 +1,234 @@
 package com.example.boltdogapp;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class ViewRequests extends Fragment {
-    @Nullable
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.usb.UsbRequest;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.boltdogapp.model.Announcement;
+import com.example.boltdogapp.model.Request;
+import com.example.boltdogapp.model.User;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class ViewRequests extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+    private RelativeLayout rlLogout;
+
+    private String idUser;
+
+    private TextView tvNumeUserConectat;
+    private TextView tvEmailUserConectat;
+
+
+    private ImageView imageView;
+    private ImageView ivProfil,ivReview;
+
+    private FirebaseUser userConectat;
+    FirebaseAuth mAuth=FirebaseAuth.getInstance();
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://boltdogapp-default-rtdb.firebaseio.com/");
+    private String numeComplet;
+    private ListView listView;
+    private ArrayList<Request> arrayList= new ArrayList<>();
+    private RequestAdapter requestAdapter;
+    User user;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_requests,container,false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_requests);
+        listView=findViewById(R.id.requests_list);
+        reference.child("users").child(mAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user=snapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        reference.child("requests").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot1:snapshot.getChildren()){
+                    Request request=dataSnapshot1.getValue(Request.class);
+//                    System.out.println(announcement);
+                    if(request.getOwnername().equals(user.getLastname()+" "+user.getFirstname())){
+                        arrayList.add(request);
+                        requestAdapter.notifyDataSetChanged();
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        requestAdapter= new RequestAdapter(arrayList,ViewRequests.this);
+
+        listView.setAdapter(requestAdapter);
+        requestAdapter.notifyDataSetChanged();
+        initializeazaAtribute();
+
+        seteazaToolbar();
+
+        seteazaToggle();
+
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+        rlLogout.setOnClickListener(this);
+        ivProfil.setOnClickListener(this);
+        ivReview.setOnClickListener(this);
+
+
+        incarcaInfoNavMenu();
+    }
+
+
+    private void seteazaToggle() {
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void seteazaToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+    }
+
+    private void initializeazaAtribute() {
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        navigationView = findViewById(R.id.navigationView);
+        tvNumeUserConectat = navigationView.getHeaderView(0).findViewById(R.id.tvNumeUserConectat);
+        tvEmailUserConectat = navigationView.getHeaderView(0).findViewById(R.id.tvEmailUserConectat);
+
+
+        rlLogout = findViewById(R.id.rlLogout);
+
+        userConectat = mAuth.getCurrentUser();
+        idUser = userConectat.getUid();
+
+        ivProfil = findViewById(R.id.ivProfile);
+        ivReview= findViewById(R.id.ivReview);
+
+    }
+
+
+
+    public void incarcaInfoNavMenu() {
+        reference.child(idUser)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User user = snapshot.getValue(User.class);
+
+                        if (user != null) {
+                            String nume = user.getLastname();
+                            String prenume = user.getFirstname();
+                            numeComplet = nume + " " + prenume;
+                            String email =user.getEmail();
+
+
+                            tvNumeUserConectat.setText(numeComplet);
+
+                            tvEmailUserConectat.setText(email);
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("preluareOwner", error.getMessage());
+                    }
+                });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.ivProfile:
+                // startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                break;
+            case R.id.ivReview:
+                // startActivity(new Intent(getApplicationContext(), ReviewActivity.class));
+                break;
+
+            case R.id.rlLogout:
+                AlertDialog.Builder builder= new AlertDialog.Builder(this);
+                builder.setTitle("Logout");
+                builder.setMessage("Sunteti sigur ca vreti sa va deconectati?");
+                builder.setPositiveButton("Da", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        mAuth.signOut();
+                        startActivity(new Intent(ViewRequests.this, LoginActivity.class));
+
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Nu", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+                break;
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_add_announcements:
+                startActivity(new Intent(getApplicationContext(),addAnnouncementActivity.class));
+                break;
+            case R.id.nav_view_requests:
+                startActivity(new Intent(getApplicationContext(),ViewRequests.class));
+                break;
+        }
+        return true;
     }
 }
